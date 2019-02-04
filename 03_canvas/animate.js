3fa2815cd7981ebe5723bd68e2fe8c492f2ccfc4
Vincent Lin <vlin6@stuy.edu>
2019-02-04 14:13:29 -0500

    k03 done

---
+++ 03_canvas/animate.js
@@ -0,0 +1,45 @@
+//Team Unique Rabbit - Vincent Lin, Daniel Keriazis
+//Softdev2 pd8
+//K03 -- They lock us in the tower whenever we get caught
+//2019-2-4
+
+var c = document.getElementById("playground");
+var ctx = c.getContext("2d");
+var dotButton = document.getElementById("circle");
+var stopButton = document.getElementById("stop");
+var growing = true;
+var requestID;
+var radius = 0;
+
+var clear = () =>{
+    console.log("clear:", requestID);
+    ctx.clearRect(0, 0, c.width, c.height);
+};
+
+var drawDot = () =>{
+    requestID = window.requestAnimationFrame(drawDot);
+    clear();
+    // draw dot
+    ctx.beginPath();
+    ctx.arc(c.width / 2, c.height / 2, radius, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.fill();
+    if (growing){
+	radius ++;
+    }
+    else{radius --;}
+    if (radius >= c.width/2){
+	growing = false;
+    }
+    else if(radius == 0){
+	growing = true;
+    }
+};
+
+var stopIt = () =>{
+    console.log("stopIt:" , requestID);
+    window.cancelAnimationFrame(requestID);
+};
+
+dotButton.addEventListener("click", drawDot);
+stopButton.addEventListener("click", stopIt);
